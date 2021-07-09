61061d982b5ac5451ff707c15437d8564ec34488
AlejoPal <alejandro.palacios.apf@hotmail.com>
2021-07-08 23:50:21 -0500

    Lectura de numero de la suerte

---
+++ src/App.java
@@ -0,0 +1,25 @@
+public class App {
+
+    public static int obtenerNumeroSuerte(String fecha ){
+            int numeroSuerte = 0; 
+            String [] arreglofecha = fecha.split("/",3);
+            int valorDia = Integer.parseInt(arreglofecha[0]);
+            int valorMes = Integer.parseInt(arreglofecha[1]);
+            int valorAnio = Integer.parseInt(arreglofecha[2]);
+    
+            int sumaPartesFechas = valorDia + valorMes + valorAnio;
+            while(sumaPartesFechas > 0){
+                numeroSuerte += sumaPartesFechas%10;
+                sumaPartesFechas = sumaPartesFechas / 10;
+            }
+            return numeroSuerte;
+        }
+    
+    public static void main(String[] args) {
+        System.out.println(obtenerNumeroSuerte("12/12/1992"));
+        }
+    
+    publicstatic void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
